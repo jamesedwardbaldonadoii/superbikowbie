11cf65d23596230929e06f55d5ec94ff21f44837
James Edward Baldonado II <jamesb@tawk.to>
2021-01-06 13:53:18 +0800

    Multiple fixes on setting up for aws environment

--- logger.js
+++ logger.js
@@ -1,9 +1,9 @@
-const { Logger } = require('./lib')
-const sentryDsn = process.env.SENTRY_DSN
-const isDev = process.env.NODE_ENV === 'development'
+const { Logger } = require('./lib');
+const sentryDsn = process.env.SENTRY_DSN;
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = new Logger({
-  appName: 'baciwa-api',
+  appName: 'tabang-api',
   raw: !isDev,
   ...(!isDev && { capture: true, sentryDsn })
-})
+});
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const stdout = require('stdout-stream')
-const chalk = require('chalk')
+const stdout = require('stdout-stream');
+const chalk = require('chalk');
 
-const config = require('./config')
-const logger = require('./logger')
+const config = require('./config');
+const logger = require('./logger');
 
-const schemas = require('./models')
+const schemas = require('./models');
 
-const { Server, Mongoose } = require('./lib')
+const { Server, Mongoose } = require('./lib');
 
 config
   .rootInit()
@@ -19,17 +19,17 @@ config
       logger
     })
       .then(() => {
-        console.log('MongoDB Successfully Connected!')
+        console.log('MongoDB Successfully Connected!');
       })
       .catch((error) => {
-        stdout.write(chalk.blue(error.stack))
-        logger.error('MongoDB fails to initialize...', error)
-      })
+        stdout.write(chalk.blue(error.stack));
+        logger.error('MongoDB fails to initialize...', error);
+      });
   })
   .then(() => {
-    const controllers = require('./controllers')
-    const middlewares = require('./middlewares')
-    const errorMiddleware = require('./middlewares/errorMiddleware')
+    const controllers = require('./controllers');
+    const middlewares = require('./middlewares');
+    const errorMiddleware = require('./middlewares/errorMiddleware');
 
     return new Server({
       port: Number(config.app.port),
@@ -39,24 +39,24 @@ config
       errorMiddleware,
       cookieSecret: config.app.cookieSecret,
       logger
-    })
+    });
   })
   .then((serverParams) => {
-    logger.info('Server initialized...', serverParams)
-    logger.debug('--- APP CONFIG ---')
-    logger.debug(`HOST: ${config.app.host}`)
-    logger.debug(`PORT: ${config.app.port}`)
-    logger.debug(`NAME: ${config.app.name}`)
-    logger.debug('--- TOKENS CONFIGS ---')
-    logger.debug('REFRESH:', config.token.refresh)
-    logger.debug('ACCESS:', config.token.access.clear())
-    logger.debug('RESET PASSWORD:', config.token.resetPassword.clear())
-    logger.debug('EMAIL CONFIRM:', config.token.emailConfirm.clear())
-    logger.debug(`ISSUER: ${config.token.jwtIss}`)
+    logger.info('Server initialized...', serverParams);
+    logger.debug('--- APP CONFIG ---');
+    logger.debug(`HOST: ${config.app.host}`);
+    logger.debug(`PORT: ${config.app.port}`);
+    logger.debug(`NAME: ${config.app.name}`);
+    logger.debug('--- TOKENS CONFIGS ---');
+    logger.debug('REFRESH:', config.token.refresh);
+    logger.debug('ACCESS:', config.token.access.clear());
+    logger.debug('RESET PASSWORD:', config.token.resetPassword.clear());
+    logger.debug('EMAIL CONFIRM:', config.token.emailConfirm.clear());
+    logger.debug(`ISSUER: ${config.token.jwtIss}`);
 
-    console.log(`Server up and running on port ${config.app.port} !`)
+    console.log(`Server up and running on port ${config.app.port} !`);
   })
   .catch((error) => {
-    stdout.write(chalk.blue(error.stack))
-    logger.error('Server fails to initialize...', error)
-  })
+    stdout.write(chalk.blue(error.stack));
+    logger.error('Server fails to initialize...', error);
+  });
